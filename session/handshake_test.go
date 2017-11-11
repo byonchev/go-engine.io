d615e6ddc724d0563145556efb55e79e4fae5673
Boris Yonchev <boris@yonchev.me>
2017-11-11 23:09:58 +0200

    Handshake packet test

---
+++ session/handshake_test.go
@@ -0,0 +1,30 @@
+package session
+
+import (
+	"testing"
+	"time"
+
+	"github.com/byonchev/go-engine.io/config"
+	"github.com/byonchev/go-engine.io/packet"
+	
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandshakePacket(t *testing.T) {
+	config := Config{
+		PingSettings: config.PingSettings{
+			PingInterval: 1 * time.Second,
+			PingTimeout:  2 * time.Second,
+		},
+	}
+
+	expected := packet.Packet{
+		Binary: false,
+		Type:   packet.Open,
+		Data:   []byte("{\"sid\":\"100200300\",\"upgrades\":[],\"pingTimeout\":2000,\"pingInterval\":1000}"),
+	}
+
+	actual := createHandshakePacket("100200300", config)
+
+	assert.Equal(t, expected, actual, "handshake packet is invalid")
+}
