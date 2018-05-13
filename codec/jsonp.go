2f337043369fc5a16fdd52afbb0df98aa6dc8df2
Boris Yonchev <boris@yonchev.me>
2018-05-13 23:52:15 +0300

    JSONP codec implemented

---
+++ codec/jsonp.go
@@ -0,0 +1,117 @@
+package codec
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"strings"
+
+	"github.com/byonchev/go-engine.io/packet"
+)
+
+// JSONP is a codec for encoding messages for cross-domain polling
+type JSONP struct {
+	Index    string
+	delegate XHR
+}
+
+const hexCharacters = "0123456789abcdef"
+
+// Encode encodes payload of packets for single poll
+func (codec JSONP) Encode(payload packet.Payload, writer io.Writer) error {
+	var buffer bytes.Buffer
+
+	_, err := writer.Write([]byte("___eio[" + codec.Index + "](\""))
+
+	if err != nil {
+		return err
+	}
+
+	err = codec.delegate.Encode(payload, &buffer)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = writer.Write(codec.escape(buffer.String()))
+
+	if err != nil {
+		return err
+	}
+
+	_, err = writer.Write([]byte("\");"))
+
+	return err
+}
+
+// Decode decodes payload of packets
+func (codec JSONP) Decode(reader io.Reader) (packet.Payload, error) {
+	data, err := ioutil.ReadAll(reader)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) < 2 {
+		return nil, errors.New("invalid form data")
+	}
+
+	query, err := url.QueryUnescape(string(data[2:]))
+
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := bytes.NewBufferString(codec.unescape(query))
+
+	return codec.delegate.Decode(buffer)
+}
+
+func (codec JSONP) escape(data string) []byte {
+	var buffer bytes.Buffer
+
+	for _, char := range data {
+		switch char {
+		case '\\', '"', '/':
+			buffer.WriteRune('\\')
+			buffer.WriteRune(char)
+		case '\b':
+			buffer.WriteRune('\\')
+			buffer.WriteRune('b')
+		case '\f':
+			buffer.WriteRune('\\')
+			buffer.WriteRune('f')
+		case '\n':
+			buffer.WriteRune('\\')
+			buffer.WriteRune('n')
+		case '\r':
+			buffer.WriteRune('\\')
+			buffer.WriteRune('r')
+		case '\t':
+			buffer.WriteRune('\\')
+			buffer.WriteRune('t')
+		case '\u2028', '\u2029':
+			buffer.WriteString("\\u202")
+			buffer.WriteByte(hexCharacters[char&0xF])
+		default:
+			if char < 0x20 || char == 0x2028 || char == 0x2029 {
+				buffer.WriteString("\\u00")
+				buffer.WriteByte(hexCharacters[char>>4])
+				buffer.WriteByte(hexCharacters[char&0xF])
+			} else {
+				buffer.WriteRune(char)
+			}
+		}
+	}
+
+	return buffer.Bytes()
+}
+
+func (codec JSONP) unescape(data string) string {
+	data = strings.Replace(data, `\\\\n`, "\\n", -1)
+	data = strings.Replace(data, `\\n`, "\n", -1)
+
+	return data
+}
---
+++ codec/jsonp_test.go
@@ -0,0 +1,137 @@
+package codec_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/byonchev/go-engine.io/codec"
+	"github.com/byonchev/go-engine.io/packet"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJSONPEncode(t *testing.T) {
+	codec := codec.JSONP{Index: "0"}
+
+	tests := []struct {
+		payload packet.Payload
+		encoded string
+	}{
+		{
+			packet.Payload{
+				packet.NewStringMessage("javascript-safe \n \" / \u0015 \u516B\u2028\u2029"),
+			},
+			`___eio[0]("28:4javascript-safe \n \" \/ \u0015 八\u2028\u2029");`,
+		},
+		{
+			packet.Payload{
+				packet.NewOpen([]byte("hello")),
+				packet.NewStringMessage("world"),
+			},
+			"___eio[0](\"6:0hello6:4world\");",
+		},
+		{
+			packet.Payload{
+				packet.NewBinaryMessage([]byte{2, 4, 8}),
+			},
+			"___eio[0](\"6:b4AgQI\");",
+		},
+		{
+			packet.Payload{
+				packet.NewClose(),
+				packet.NewBinaryMessage([]byte{42}),
+			},
+			"___eio[0](\"1:16:b4Kg==\");",
+		},
+	}
+
+	for _, test := range tests {
+		var buffer bytes.Buffer
+
+		err := codec.Encode(test.payload, &buffer)
+
+		actual := buffer.String()
+		expected := test.encoded
+
+		assert.Nil(t, err, "error while encoding payload")
+		assert.Equal(t, expected, actual, "payload was not encoded propery")
+	}
+}
+
+func TestJSONPDecode(t *testing.T) {
+	codec := codec.JSONP{}
+
+	tests := []struct {
+		data    []byte
+		decoded packet.Payload
+	}{
+		{
+			[]byte(`d=4:4\\n\\\\n`),
+			packet.Payload{
+				packet.NewStringMessage("\n\\n"),
+			},
+		},
+		{
+			[]byte("d=6:4hello"),
+			packet.Payload{
+				packet.NewStringMessage("hello"),
+			},
+		},
+		{
+			[]byte("d=6:4hello6:4world6:3probe"),
+			packet.Payload{
+				packet.NewStringMessage("hello"),
+				packet.NewStringMessage("world"),
+				packet.NewPong([]byte("probe")),
+			},
+		},
+		{
+			[]byte("d=6:b4AgQI"),
+			packet.Payload{
+				packet.NewBinaryMessage([]byte{2, 4, 8}),
+			},
+		},
+		{
+			[]byte("d=1:16:b4Kg=="),
+			packet.Payload{
+				packet.NewClose(),
+				packet.NewBinaryMessage([]byte{42}),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		actual, err := codec.Decode(bytes.NewBuffer(test.data))
+		expected := test.decoded
+
+		assert.Nil(t, err, "error while decoding valid payload")
+		assert.Equal(t, expected, actual, "payload was not decoded properly")
+	}
+}
+
+func BenchmarkJSONPEncode(b *testing.B) {
+	codec := codec.JSONP{Index: "0"}
+
+	payload := packet.Payload{
+		packet.NewOpen([]byte("hello")),
+		packet.NewStringMessage("world"),
+		packet.NewBinaryMessage([]byte{'!'}),
+	}
+
+	var buffer bytes.Buffer
+
+	for n := 0; n < b.N; n++ {
+		codec.Encode(payload, &buffer)
+	}
+}
+
+func BenchmarkJSONPDecode(b *testing.B) {
+	codec := codec.JSONP{}
+
+	data := []byte("d=6:0hello6:4world6:b4IQ==")
+
+	buffer := bytes.NewBuffer(data)
+
+	for n := 0; n < b.N; n++ {
+		codec.Decode(buffer)
+	}
+}
