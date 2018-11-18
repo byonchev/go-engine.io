58b97e20ec26f78194583e5ae256d2a3a5c21020
Boris Yonchev <boris@yonchev.me>
2018-11-18 21:59:46 +0200

    Changed UUID generator to gofrs/uuid (#11)

--- internal/utils/id.go
+++ internal/utils/id.go
@@ -3,12 +3,12 @@ package utils
 import (
 	"encoding/base64"
 
-	"github.com/satori/go.uuid"
+	"github.com/gofrs/uuid"
 )
 
 // GenerateBase64ID generates UUID v4 and encodes it into base64 without padding
 func GenerateBase64ID() string {
-	uuid, _ := uuid.NewV4()
+	uuid := uuid.Must(uuid.NewV4())
 
 	return base64.RawURLEncoding.EncodeToString(uuid.Bytes())
 }
